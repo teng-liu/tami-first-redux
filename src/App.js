c1f1f08104b552ac27d69ff1b7ccf35551d87533
Tami Liu <tliu@GOV.PE.CA>
2019-05-10 13:17:17 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Posts from './components/Posts';
+import Postform from './components/Postform';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Provider } from 'react-redux';
+// import {createStore, applyMiddleware} from 'redux';
+import store from './store';
+
+// referenced from: https://www.youtube.com/watch?v=93p3LxR9xfM
+
+// const store = createStore(() => [], {}, applyMiddleware());
+
+class App extends Component {
+  render(){
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Posts.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {fetchPosts} from '../actions/postActions';
+// rcc  - class, rfc - function
+class Posts extends Component {
+
+  componentWillMount(){
+    this.props.fetchPosts();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.newPost){
+      this.props.posts.unshift(nextProps.newPost);  // .push
+    }
+  }
+  
+  render() {
+    const postItems = this.props.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
+    
+    return (
+      <div>
+        <h1>post-example</h1>
+        {postItems}
+      </div>
+    )
+  }
+}
+
+// export default Post;
+
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+  newPost: PropTypes.object
+}
+
+const mapStateToProps = state => ({
+  posts: state.posts.items,
+  newPost: state.posts.item
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
